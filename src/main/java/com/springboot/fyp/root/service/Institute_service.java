3e6393e3829660aae8ecd8fc15063b500fa125db
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-01-01 05:05:12 +0500

    Redis functionality added in Institute service.

--- src/main/java/com/springboot/fyp/root/service/Institute_service.java
+++ src/main/java/com/springboot/fyp/root/service/Institute_service.java
@@ -18,16 +18,29 @@ public class Institute_service {
 	@Autowired
 	SequenceGeneratorService sequenceGeneratorService;
 	
+	public static final String HASH_KEY_INSTITUTES_LIST = "InstitutesList";
+	
+	@Autowired
+	RedisUtilityRoot redisUtilityRoot;
+	
 	public String insert(Institute institute){
 		institute.setInstitute_id(sequenceGeneratorService.getSequenceNumber(institute.SEQUENCE_NAME));
 		institute_repository.insert(institute);
+		redisUtilityRoot.deleteList(HASH_KEY_INSTITUTES_LIST);
 		return institute.getInstitute_name();
 	}
 	
+	@SuppressWarnings("unchecked")
 	public List<Institute> getAll(){
-		if(institute_repository.findAll().isEmpty()) {
-			return new ArrayList<>();
+		if(redisUtilityRoot.getList(HASH_KEY_INSTITUTES_LIST).size() > 0) {
+			return redisUtilityRoot.getList(HASH_KEY_INSTITUTES_LIST);
+		}else {
+			if(institute_repository.findAll().isEmpty()) {
+				return new ArrayList<>();
+			}
+			redisUtilityRoot.saveList(institute_repository.findAll(), HASH_KEY_INSTITUTES_LIST);
+			return redisUtilityRoot.getList(HASH_KEY_INSTITUTES_LIST);
 		}
-		return institute_repository.findAll();
+		
 	}
 }
