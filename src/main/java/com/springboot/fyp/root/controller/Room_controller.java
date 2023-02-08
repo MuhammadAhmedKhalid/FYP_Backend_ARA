2a85e55ed05db415a3ddd1e9d2427a255f3eff14
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-02-08 22:25:25 +0500

    Redis functionality added for Get and add room endpoint.

--- src/main/java/com/springboot/fyp/root/controller/Room_controller.java
+++ src/main/java/com/springboot/fyp/root/controller/Room_controller.java
@@ -3,8 +3,10 @@ package com.springboot.fyp.root.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +26,13 @@ public class Room_controller {
 		return ResponseEntity.ok(response);
 	}
 	
-	@GetMapping("/rooms")
-	public ResponseEntity<List<Room>> getRooms(){
-		return ResponseEntity.ok(room_service.getAll());
+	@GetMapping("/rooms/{institute_id}")
+	public ResponseEntity<List<Room>> getRooms(@PathVariable("institute_id") int institute_id){
+		List<Room> rooms_lst = room_service.getAll(institute_id);
+		if(rooms_lst == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		return ResponseEntity.ok(rooms_lst);
 	}
 	
 }
--- src/main/java/com/springboot/fyp/root/service/Room_service.java
+++ src/main/java/com/springboot/fyp/root/service/Room_service.java
@@ -1,5 +1,6 @@
 package com.springboot.fyp.root.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,15 +17,38 @@ public class Room_service {
 	
 	@Autowired
 	SequenceGeneratorService sequenceGeneratorService;
+	
+	@Autowired
+	RedisUtilityRoot redisUtilityRoot;
+	
+	public static final String HASH_KEY_ROOMS_LIST = "RoomsList";
 
 	public String insert(Room room) {
 		room.setRoom_id(sequenceGeneratorService.getSequenceNumber(room.SEQUENCE_NAME));
 		room_repository.insert(room);
+		redisUtilityRoot.deleteList(HASH_KEY_ROOMS_LIST+room.getInstitute_id());
 		return "Operation performed successfully.";
 	}
 	
-	public List<Room> getAll(){
-		return room_repository.findAll();
+	@SuppressWarnings("unchecked")
+	public List<Room> getAll(int institute_id){
+		List<Room> rooms = redisUtilityRoot.getList(HASH_KEY_ROOMS_LIST+institute_id);
+		if(rooms.size() > 0) {
+			return rooms;
+		}else {
+			if(room_repository.findAll().isEmpty()) {
+				return null;
+			} else {
+				List<Room> rooms_lst = new ArrayList<>();
+				for(Room room : room_repository.findAll()) {
+					if(room.getInstitute_id() == institute_id) {
+						rooms_lst.add(room);
+					}
+				}
+				redisUtilityRoot.saveList(rooms_lst, HASH_KEY_ROOMS_LIST+institute_id);
+				return rooms_lst;
+			}
+		}
 	}
 	
 }
