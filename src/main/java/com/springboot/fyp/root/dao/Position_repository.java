2ecc0085a3a7cee51574af8e1954f193739f8b39
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-02-11 02:26:16 +0500

    Repositories created for specialization and position.

--- src/main/java/com/springboot/fyp/root/dao/Position_repository.java
+++ src/main/java/com/springboot/fyp/root/dao/Position_repository.java
@@ -3,9 +3,7 @@ package com.springboot.fyp.root.dao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.springboot.fyp.root.models.Object_Request;
+import com.springboot.fyp.root.models.Position;
 
 @Repository
-public interface Object_request_repository extends MongoRepository<Object_Request, Integer> {
-
-}
+public interface Position_repository extends MongoRepository<Position, Integer> {}
--- src/main/java/com/springboot/fyp/root/dao/Specialization_repository.java
+++ src/main/java/com/springboot/fyp/root/dao/Specialization_repository.java
@@ -3,9 +3,7 @@ package com.springboot.fyp.root.dao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.springboot.fyp.root.models.Object_Request;
+import com.springboot.fyp.root.models.Specialization;
 
 @Repository
-public interface Object_request_repository extends MongoRepository<Object_Request, Integer> {
-
-}
+public interface Specialization_repository extends MongoRepository<Specialization, Integer>{}
