1af2de2d9df3af149a4b4fbe64e0ad88a4aa0481
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-02-11 02:53:55 +0500

    Endpoints created for Position model class.

--- src/main/java/com/springboot/fyp/root/controller/Position_controller.java
+++ src/main/java/com/springboot/fyp/root/controller/Position_controller.java
@@ -1,8 +1,16 @@
 package com.springboot.fyp.root.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.fyp.root.models.Position;
 import com.springboot.fyp.root.service.Position_service;
 
 @RestController
@@ -10,5 +18,20 @@ public class Position_controller {
 
 	@Autowired
 	Position_service position_service;
+	
+	@PostMapping("/addPosition")
+	public ResponseEntity<String> addPosition(@RequestBody Position position){
+		String response = position_service.add(position);
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/getPositions")
+	public ResponseEntity<List<Position>> getPositions(){
+		List<Position> positions = position_service.getAll();
+		if(positions == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		return ResponseEntity.ok(positions);
+	}
 	
 }
--- src/main/java/com/springboot/fyp/root/service/Position_service.java
+++ src/main/java/com/springboot/fyp/root/service/Position_service.java
@@ -1,9 +1,12 @@
 package com.springboot.fyp.root.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.fyp.root.dao.Position_repository;
+import com.springboot.fyp.root.models.Position;
 
 @Service
 public class Position_service {
@@ -13,5 +16,19 @@ public class Position_service {
 	
 	@Autowired
 	SequenceGeneratorService sequenceGeneratorService;
+	
+	public String add(Position position) {
+		position.setPosition_id(sequenceGeneratorService.getSequenceNumber(position.SEQUENCE_NAME));
+		position_repository.insert(position);
+		return "Operation performed successfully.";
+	}
+	
+	public List<Position> getAll(){
+		List<Position> positions = position_repository.findAll();
+		if(positions.isEmpty()) {
+			return null;
+		}
+		return positions;
+	}
 	
 }
