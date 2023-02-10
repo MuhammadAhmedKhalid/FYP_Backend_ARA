f249f621e30b8e909bf05cc0ec96fc827b484c65
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-02-11 04:55:56 +0500

    Endpoints created for Specialization model class.

--- src/main/java/com/springboot/fyp/root/controller/Specialization_controller.java
+++ src/main/java/com/springboot/fyp/root/controller/Specialization_controller.java
@@ -1,8 +1,16 @@
 package com.springboot.fyp.root.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.fyp.root.models.Specialization;
 import com.springboot.fyp.root.service.Specialization_service;
 
 @RestController
@@ -10,5 +18,20 @@ public class Specialization_controller {
 	
 	@Autowired
 	Specialization_service specialization_service;
+	
+	@PostMapping("/addSpecialization")
+	public ResponseEntity<String> addSpecialization(@RequestBody Specialization specialization){
+		String response = specialization_service.add(specialization);
+		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/getSpecializations")
+	public ResponseEntity<List<Specialization>> getSpecializations(){
+		List<Specialization> specializations = specialization_service.getAll();
+		if(specializations == null) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		return ResponseEntity.ok(specializations);
+	}
 	
 }
--- src/main/java/com/springboot/fyp/root/service/Specialization_service.java
+++ src/main/java/com/springboot/fyp/root/service/Specialization_service.java
@@ -1,9 +1,12 @@
 package com.springboot.fyp.root.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.fyp.root.dao.Specialization_repository;
+import com.springboot.fyp.root.models.Specialization;
 
 @Service
 public class Specialization_service {
@@ -13,5 +16,19 @@ public class Specialization_service {
 	
 	@Autowired
 	SequenceGeneratorService sequenceGeneratorService;
+	
+	public String add(Specialization specialization) {
+		specialization.setSpecialization_id(sequenceGeneratorService.getSequenceNumber(specialization.SEQUENCE_NAME));
+		specialization_repository.insert(specialization);
+		return "Operation performed successfully.";
+	}
+	
+	public List<Specialization> getAll(){
+		List<Specialization> specializations = specialization_repository.findAll();
+		if(specializations.isEmpty()) {
+			return null;
+		}
+		return specializations;
+	}
 	
 }
