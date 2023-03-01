4aa117052a218d3eab26da61345f1ae0f1504db9
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-03-01 18:43:08 +0500

    Object request service class modified.

--- src/main/java/com/springboot/fyp/root/service/Object_request_service.java
+++ src/main/java/com/springboot/fyp/root/service/Object_request_service.java
@@ -27,13 +27,11 @@ public class Object_request_service {
 	public static final String HASH_KEY_OBJECT_REQUESTS = "ObjectRequests";
 		
 	public String add(Object_Request object_Request) {
-		
 		object_Request.setObj_req_id(sequenceGeneratorService.getSequenceNumber(object_Request.SEQUENCE_NAME));
 		int available_quantity = non_living_resource_service.getQuantity(object_Request.getResource_type_id());
 		if(object_Request.getQuantity() > available_quantity) {			
 			return null;
 		}
-		
 		object_request_repository.insert(object_Request);
 		redisUtilityRoot.deleteList(HASH_KEY_OBJECT_REQUESTS+object_Request.getInstitute_id());
 		return "Operation performed successfully.";
@@ -50,7 +48,6 @@ public class Object_request_service {
 					objectRequests.add(requests);
 				}
 			}
-			
 			redisUtilityRoot.saveList(objectRequests, HASH_KEY_OBJECT_REQUESTS+institute_id);
 			return objectRequests;
 		}
