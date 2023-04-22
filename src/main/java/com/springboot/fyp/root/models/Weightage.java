02add57500e000d1f172188f1e59d82de7dc6080
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-04-22 19:15:12 +0500

    Weightage model class updated.

--- src/main/java/com/springboot/fyp/root/models/Weightage.java
+++ src/main/java/com/springboot/fyp/root/models/Weightage.java
@@ -8,6 +8,7 @@ import lombok.Data;
 public class Weightage {
 	
 	private int institute_id;
+	private boolean isSelected;
 	private List<List<JaccardResult>> jaccardResults;
 	private List<AssignedCourse> assignedCourse;
 	
