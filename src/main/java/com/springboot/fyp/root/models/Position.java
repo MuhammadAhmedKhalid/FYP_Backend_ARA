9cb75f1d14e54e149c9ff447fc5b16b4b8ff51c9
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-04-27 22:33:00 +0500

    Position model class updated.

--- src/main/java/com/springboot/fyp/root/models/Position.java
+++ src/main/java/com/springboot/fyp/root/models/Position.java
@@ -1,5 +1,6 @@
 package com.springboot.fyp.root.models;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,6 +13,7 @@ public class Position {
 	@Transient
 	public final String SEQUENCE_NAME="Position_sequence";
 	
+	@Id
 	private int position_id;
 	private String position_name;
 	private int institute_id;
