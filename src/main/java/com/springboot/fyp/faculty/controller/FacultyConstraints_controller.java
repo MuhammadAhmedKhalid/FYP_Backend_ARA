d4ae51fe0911faf97769ec2fb1d74357979660b6
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-13 02:48:55 +0500

    Get faculty constraints end-point has been created.

--- src/main/java/com/springboot/fyp/faculty/controller/FacultyConstraints_controller.java
+++ src/main/java/com/springboot/fyp/faculty/controller/FacultyConstraints_controller.java
@@ -1,7 +1,12 @@
 package com.springboot.fyp.faculty.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +24,15 @@ public class FacultyConstraints_controller {
 	public ResponseEntity<String> addFacultyConstraints(@RequestBody FacultyConstraints facultyConstraints){
 		String response = facultyConstraints_service.add(facultyConstraints);
 		return ResponseEntity.ok(response);
+	}
+	
+	@GetMapping("/getFacultyConstraints/{institute_id}")
+	public ResponseEntity<List<FacultyConstraints>> getFacultyConstraints(@PathVariable("institute_id") int institute_id){
+		List<FacultyConstraints> constraints = facultyConstraints_service.getAll(institute_id);
+		if(constraints == null) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+		}
+		return ResponseEntity.ok(constraints);
 	}
 	
 }
--- src/main/java/com/springboot/fyp/faculty/service/FacultyConstraints_service.java
+++ src/main/java/com/springboot/fyp/faculty/service/FacultyConstraints_service.java
@@ -1,10 +1,14 @@
 package com.springboot.fyp.faculty.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.fyp.faculty.dao.FacultyConstraints_repository;
 import com.springboot.fyp.faculty.models.FacultyConstraints;
+import com.springboot.fyp.root.service.RedisUtilityRoot;
 import com.springboot.fyp.root.service.SequenceGeneratorService;
 
 @Service
@@ -16,13 +20,38 @@ public class FacultyConstraints_service {
 	@Autowired
 	FacultyConstraints_repository facultyConstraints_repository;
 	
+	public static final String HASH_KEY_FACULTY_CONSTRAINTS_LIST = "FacultyConstraintsList";
+	
+	@Autowired
+	RedisUtilityRoot redisUtilityRoot;
+	
 	public String add(FacultyConstraints facultyConstraints) {
 		
 		facultyConstraints.setFacultyConstraintId(
 				sequenceGeneratorService.getSequenceNumber(facultyConstraints.SEQUENCE_NAME));
 		facultyConstraints_repository.insert(facultyConstraints);
 		
+		redisUtilityRoot.deleteList(HASH_KEY_FACULTY_CONSTRAINTS_LIST+facultyConstraints.getInstitute_id());
 		return "Operation performed successfully.";
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<FacultyConstraints> getAll(int institute_id){
+		if(redisUtilityRoot.getList(HASH_KEY_FACULTY_CONSTRAINTS_LIST+institute_id).size() > 0) {
+			return redisUtilityRoot.getList(HASH_KEY_FACULTY_CONSTRAINTS_LIST+institute_id);
+		} else {
+			if(facultyConstraints_repository.findAll().isEmpty()) {
+				return null;
+			}
+			List<FacultyConstraints> constraints = new ArrayList<>();
+			for(FacultyConstraints constraint : facultyConstraints_repository.findAll()) {
+				if(constraint.getInstitute_id() == institute_id) {
+					constraints.add(constraint);
+				}
+			}
+			redisUtilityRoot.saveList(constraints, HASH_KEY_FACULTY_CONSTRAINTS_LIST+institute_id);
+			return redisUtilityRoot.getList(HASH_KEY_FACULTY_CONSTRAINTS_LIST+institute_id);
+		}
 	}
 
 }
