36e2f5b7904f63b4cc40b18453bae7f58124c0c5
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-22 01:43:57 +0500

    From new branch

--- src/main/java/com/springboot/fyp/FypApplication.java
+++ src/main/java/com/springboot/fyp/FypApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FypApplication {
 
-	// from new branch
+	// from new branch 
 	public static void main(String[] args) {
 		SpringApplication.run(FypApplication.class, args);
 	}
