dd2cd34ffeede78e78bf54b737309c5265ad280e
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-25 02:13:42 +0500

    Offer course model class has been created.

---
+++ src/main/java/com/springboot/fyp/root/models/OfferCourse.java
@@ -0,0 +1,24 @@
+package com.springboot.fyp.root.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Document(collection = "OfferCourse")
+@Data
+public class OfferCourse {
+	
+	@Transient
+	public final String SEQUENCE_NAME="OfferCourse_sequence";
+	
+	@Id
+	private int offerCourseId;
+	private int course_id;
+	private int batchId;
+	private int department_id;
+	private int semester;
+	private int institute_id;
+
+}
